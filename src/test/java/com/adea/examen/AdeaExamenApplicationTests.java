b589fc7763ccda5d3d3db76ee60be4c2c621b05e
jesusTM <jesus.terrazas@autozone.com>
2021-11-10 21:19:55 -0700

    subir proyecto

---
+++ src/test/java/com/adea/examen/AdeaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adea.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdeaExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
